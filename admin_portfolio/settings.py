b9e043950bf0fc35a0b159ccce84ca74a8f3eeb3
Melkibson <melkiyamna@protonmail.com>
2022-08-22 12:24:15 +0200

    Add mime types

--- admin_portfolio/settings.py
+++ admin_portfolio/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("application/js", ".js", True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jytqo(2z0@!er3als-xa0g&kjozwt5dpb%#ati=ui4$_ejkfw-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.yamna.fr', '127.0.0.1']
 
--- admin_portfolio/wsgi.py
+++ admin_portfolio/wsgi.py
@@ -8,11 +8,19 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whois import whois
+from admin_portfolio.asgi import application as asgi_application
 from django.core.wsgi import get_wsgi_application
 
-from admin_portfolio.asgi import application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_portfolio.settings')
 
-app = application
+try:
+    whois('127.0.0.1')
+    application = get_wsgi_application()
+except Exception as e:
+    print(e)
+else:
+    app = asgi_application
+
+
