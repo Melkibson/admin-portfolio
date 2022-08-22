d8d6968daf66a2ec096b9204580dbb42594ef34c
Melkibson <melkiyamna@protonmail.com>
2022-08-22 10:13:42 +0200

    Update requirements.txt

--- admin_portfolio/settings.py
+++ admin_portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-jytqo(2z0@!er3als-xa0g&kjozwt5dpb%#ati=ui4$_ejkfw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.yamna.fr']
+ALLOWED_HOSTS = ['.vercel.app', '.yamna.fr', '127.0.0.1']
 
 # Application definition
 
--- admin_portfolio/wsgi.py
+++ admin_portfolio/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from admin_portfolio.asgi import application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_portfolio.settings')
 
-app = application
+application = get_wsgi_application()
