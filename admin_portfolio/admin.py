ff5df8642a5096350f9eabbbfd7fa3fdbec48a6c
Melkibson <melkiyamna@protonmail.com>
2022-08-26 20:05:49 +0200

    update .gitignore

--- admin_portfolio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, Skill, Work, NavLink, SocialLink, WorkProject
-
-admin.site.register([Project, Work, NavLink, SocialLink, Skill, WorkProject])
-
-
